88e0cbce18eecb0d74aa3f6a7221b86d12354a3d
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-25 11:40:36 +0530

    use switch statement

---
+++ switch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empwageday9
+{
+    internal class @switch
+    {
+        public static void usecases()
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(3);
+            switch (empcheck)
+            {
+                case IS_FULL_TIME:
+                    Console.WriteLine("employee is full time");
+                    emphrs = 8;
+                    break;
+
+                case IS_PART_TIME:
+                    Console.WriteLine("employee is part time");
+                    emphrs = 4;
+                    break;
+
+                default:
+                    Console.WriteLine("employee is absent");
+                    emphrs = 0;
+                    break;
+
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp wage:" + empwage);
+        }
+    }
+}
