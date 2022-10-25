22d80640013b80de6c1cd11325ff77a1e5c13ca0
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-25 11:24:10 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace empwageday9
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage problem");
+            attendance.CheckAttendance();
+            dailywage.calwage();
+            parttime.isparttime();
+            @switch.usecases();
+            monthlywage.monthwages();
+            totalworkinghours.conditions();
+        }
+    }
+}
